91c6c87399d74e1e6c901fe3ba08f34818df2526
rav-90 <rwawszczak@gmail.com>
2014-01-22 11:10:42 -0800

    Added Expertsystem to RecognizingDelegate

--- poker-bot/poker-bot-new-ui/src/main/java/pl/wroc/pwr/student/softcomputing/ui/util/RecognizingDelegate.java
+++ poker-bot/poker-bot-new-ui/src/main/java/pl/wroc/pwr/student/softcomputing/ui/util/RecognizingDelegate.java
@@ -1,6 +1,10 @@
 package pl.wroc.pwr.student.softcomputing.ui.util;
 
+import pl.wroc.pwr.student.softcomputing.pokerbot.converter.ConvertedData;
 import pl.wroc.pwr.student.softcomputing.pokerbot.converter.Converter;
+import pl.wroc.pwr.student.softcomputing.pokerbot.expertSystem.Engine;
+import pl.wroc.pwr.student.softcomputing.pokerbot.expertSystem.ExpertSystem;
+import pl.wroc.pwr.student.softcomputing.pokerbot.expertSystem.Fact;
 import pl.wroc.pwr.student.softcomputing.teacher.api.TeacherFacade;
 import pl.wroc.pwr.student.softcomputing.teacher.api.model.Fold;
 import pl.wroc.pwr.student.softcomputing.teacher.api.model.ImageConfig;
@@ -55,8 +59,16 @@ public class RecognizingDelegate {
         }
 
         Converter converter = new Converter(table.getFoldButtonStatus()== Fold.ACTIVE,table.getDealerPosition(),table.getFirstCard().toString(),table.getSecondCard().toString(),totalChips,chipsAtTable, borders);
-
-        return table.smallReport()+"\n"+converter.convertData();
+        ConvertedData convertedData = converter.convertData();
+        Engine engine = new ExpertSystem().getBasicStrategyEngine(convertedData);
+        String answer="";
+        if(engine.performBackwardChaining(new Fact("Raise"))){
+            answer="Player should RAISE in this situation!";
+        }else{
+            answer="Player should FOLD in this situation!";
+        }
+        String engineOutput = engine.getOutPut();
+        return table.smallReport()+"\n"+convertedData+"\n\n"+engineOutput+"\n"+answer;
     }
 
 	public static String recognizeAll(List<File> allFiles,
